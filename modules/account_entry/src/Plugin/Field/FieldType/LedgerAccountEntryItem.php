5f080373d992fd30e250c7a4c058cf2cea465d79
Michael Stenta <mike@mstenta.net>
2021-09-20 19:23:20 -0400

    Add Account entry field type.
    Generated with drush generate field-type:
    
    $ drush generate field-type
    
     Welcome to plugin-field-type generator!
    –––––––––––––––––––––––––––––––––––––––––
    
     Module machine name:
     ➤ ledger_account_entry
    
     Module name:
     ➤ Account entry
    
     Plugin label [Example]:
     ➤ Account entry
    
     Plugin ID [ledger_account_entry_account_entry]:
     ➤ ledger_account_entry
    
     Plugin class [LedgerAccountEntryItem]:
     ➤
    
     Make the field storage configurable? [No]:
     ➤
    
     Make the field instance configurable? [No]:
     ➤
    
     The following directories and files have been created or updated:
    –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
     • modules/account_entry/config/schema/ledger_account_entry.schema.yml
     • modules/account_entry/src/Plugin/Field/FieldType/LedgerAccountEntryItem.php

---
+++ modules/account_entry/src/Plugin/Field/FieldType/LedgerAccountEntryItem.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Drupal\ledger_account_entry\Plugin\Field\FieldType;
+
+use Drupal\Component\Utility\Random;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Field\FieldItemBase;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\TypedData\DataDefinition;
+
+/**
+ * Defines the 'ledger_account_entry' field type.
+ *
+ * @FieldType(
+ *   id = "ledger_account_entry",
+ *   label = @Translation("Account entry"),
+ *   category = @Translation("General"),
+ *   default_widget = "string_textfield",
+ *   default_formatter = "string"
+ * )
+ *
+ * @DCG
+ * If you are implementing a single value field type you may want to inherit
+ * this class form some of the field type classes provided by Drupal core.
+ * Check out /core/lib/Drupal/Core/Field/Plugin/Field/FieldType directory for a
+ * list of available field type implementations.
+ */
+class LedgerAccountEntryItem extends FieldItemBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEmpty() {
+    $value = $this->get('value')->getValue();
+    return $value === NULL || $value === '';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
+
+    // @DCG
+    // See /core/lib/Drupal/Core/TypedData/Plugin/DataType directory for
+    // available data types.
+    $properties['value'] = DataDefinition::create('string')
+      ->setLabel(t('Text value'))
+      ->setRequired(TRUE);
+
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConstraints() {
+    $constraints = parent::getConstraints();
+
+    $constraint_manager = \Drupal::typedDataManager()->getValidationConstraintManager();
+
+    // @DCG Suppose our value must not be longer than 10 characters.
+    $options['value']['Length']['max'] = 10;
+
+    // @DCG
+    // See /core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint
+    // directory for available constraints.
+    $constraints[] = $constraint_manager->create('ComplexData', $options);
+    return $constraints;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function schema(FieldStorageDefinitionInterface $field_definition) {
+
+    $columns = [
+      'value' => [
+        'type' => 'varchar',
+        'not null' => FALSE,
+        'description' => 'Column description.',
+        'length' => 255,
+      ],
+    ];
+
+    $schema = [
+      'columns' => $columns,
+      // @DCG Add indexes here if necessary.
+    ];
+
+    return $schema;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function generateSampleValue(FieldDefinitionInterface $field_definition) {
+    $random = new Random();
+    $values['value'] = $random->word(mt_rand(1, 50));
+    return $values;
+  }
+
+}
